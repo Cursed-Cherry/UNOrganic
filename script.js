dac3cb7a35354763031c2bbcb5d50621bf93c50b
Inevitable_Indra <100020514+Cursed-Cherry@users.noreply.github.com>
2026-08-21 22:34:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ let chemicals = {
         price: 100,
         demand: 24
     },
+
     B: {
         price: 150,
         demand: 13
     },
+
     C: {
         price: 80,
         demand: 41
     }
 };
+
 
 function buyChemical(chemical) {
 
@@ -20,15 +23,11 @@ function buyChemical(chemical) {
     chemicals[chemical].price =
         Math.round(chemicals[chemical].price * 1.05);
 
-    console.log(
-        "Bought Chemical " + chemical
-    );
+    document.getElementById("price-" + chemical).textContent =
+        "₹" + chemicals[chemical].price;
 
-    console.log(
-        "New price: ₹" + chemicals[chemical].price
-    );
+    document.getElementById("demand-" + chemical).textContent =
+        chemicals[chemical].demand;
 
-    console.log(
-        "Demand: " + chemicals[chemical].demand
-    );
+    console.log("Chemical " + chemical + " purchased!");
 }
