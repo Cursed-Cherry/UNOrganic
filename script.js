062f459b06d05f548fbd619ca2f580e498ebb269
Inevitable_Indra <100020514+Cursed-Cherry@users.noreply.github.com>
2026-08-21 22:25:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+let chemicals = {
+    A: {
+        price: 100,
+        demand: 24
+    },
+    B: {
+        price: 150,
+        demand: 13
+    },
+    C: {
+        price: 80,
+        demand: 41
+    }
+};
 
+function buyChemical(chemical) {
+
+    chemicals[chemical].demand += 1;
+
+    chemicals[chemical].price =
+        Math.round(chemicals[chemical].price * 1.05);
+
+    console.log(
+        "Bought Chemical " + chemical
+    );
+
+    console.log(
+        "New price: ₹" + chemicals[chemical].price
+    );
+
+    console.log(
+        "Demand: " + chemicals[chemical].demand
+    );
+}
